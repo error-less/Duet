3b6036ef26e0f9d543e4e4c0b36ac4b2db63fecf
error-less <57638183+error-less@users.noreply.github.com>
2021-07-12 12:47:30 +0530

    script changed

--- public/JS/script.js
+++ public/JS/script.js
@@ -12,6 +12,7 @@ const myPeer = new Peer(undefined, {
   host: '/',
   port: '443'
 })
+// creating video element to show on screen
 const myVideo = document.createElement('video')
 myVideo.muted = true
 
@@ -19,23 +20,27 @@ myVideo.muted = true
 const peers = {}
 let myVideoStream;
 
-// webRTC asking permision for audio and video
+// webRTC asking permision for audio and video**********************************************
 navigator.mediaDevices.getUserMedia({
   video: true,
   audio: true
 }).then(stream => {
   myVideoStream = stream;
+  // passing video stream to client side
   addVideoStream(myVideo, stream)
 
+  // answering to call when new user joined
   myPeer.on('call', call => {
     call.answer(stream)
+    // creating video elements 
     const video = document.createElement('video')
     call.on('stream', userVideoStream => {
       addVideoStream(video, userVideoStream)
     })
   })
+  // **********************************************************************************************
 
-  // user connected
+  // user connected-----------------------------------------------
   socket.on('user-connected', userId => {
     connectToNewUser(userId, stream)
     // time delay for video display
@@ -44,11 +49,12 @@ navigator.mediaDevices.getUserMedia({
 
 })
 
-// user disconnected to client side
+// user disconnected to client side------------------------------------------
 socket.on('user-disconnected', userId => {
   if (peers[userId]) peers[userId].close()
 })
 
+// ending the call from client side-----------------------------------------
 endcall.addEventListener("click", ()=>{
   location.href='/'
 })
@@ -59,7 +65,7 @@ myPeer.on('open', id => {
   socket.emit('join-room', ROOM_ID, id,user)
 })
 
-// connecting to new user
+// connecting to new user--------------------------------------------------------------
 function connectToNewUser(userId, stream) {
   const call = myPeer.call(userId, stream)
   const video = document.createElement('video')
@@ -75,15 +81,18 @@ function connectToNewUser(userId, stream) {
   peers[userId] = call
 }
 
-// adding stream and display
+// adding stream and display----------------------------------------------------------------
 function addVideoStream(video, stream) {
   video.srcObject = stream
   video.addEventListener('loadedmetadata', () => {
     video.play()
   })
+  // adding new video to screen
   videoGrid.append(video)
 }
-console.log(videoGrid.children)
+
+// message portion #################################################################################################################
+
 let text = document.querySelector("#chat_message");
 let send = document.getElementById("send");
 let messages = document.querySelector(".messages");
@@ -104,15 +113,18 @@ text.addEventListener("keydown", (e) => {
   }
 });
 
+// message portion ends ##############################################################################################
+
+// working with buttons--------------------------------------------------------------------------------------------------
 const inviteButton = document.querySelector("#inviteButton");
 const muteButton = document.querySelector("#muteButton");
 const stopVideo = document.querySelector("#stopVideo");
 
-// mute the audio during call
+// mute the audio during call--------------------------
+
 muteButton.addEventListener("click", () => {
   const enabled = myVideoStream.getAudioTracks()[0].enabled;
   if (enabled) {
-    alert("You are muted !!");
     myVideoStream.getAudioTracks()[0].enabled = false;
     html = `<i class="fas fa-microphone-slash"></i>`;
     muteButton.classList.toggle("background__red");
@@ -125,12 +137,12 @@ muteButton.addEventListener("click", () => {
   }
 });
 
+// mute button ends ---------------------------------------------
 
-// to stop the video on clicking mute video
+// to stop the video on clicking mute video-------------------------------------------
 stopVideo.addEventListener("click", () => {
   const enabled = myVideoStream.getVideoTracks()[0].enabled;
   if (enabled) {
-    alert("Your video is stopped !!");
     myVideoStream.getVideoTracks()[0].enabled = false;
     html = `<i class="fas fa-video-slash"></i>`;
     stopVideo.classList.toggle("background__red");
@@ -142,8 +154,9 @@ stopVideo.addEventListener("click", () => {
     stopVideo.innerHTML = html;
   }
 });
+// stop video ends ---------------------------------------------------
 
-// to add more people in meeting
+// to add more people in meeting---------------------
 inviteButton.addEventListener("click", (e) => {
   prompt(
     "Copy this link and send it to people you want to meet with",
@@ -151,8 +164,10 @@ inviteButton.addEventListener("click", (e) => {
   );
 });
 
+// working with buttons closed ------------------------------------------------------------------------------------------------
 
-// getting message in client side
+// getting message in client side @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+
 socket.on("createMessage", (message,userName) => {
   messages.innerHTML =
     messages.innerHTML +
@@ -163,3 +178,4 @@ socket.on("createMessage", (message,userName) => {
         <span>${message}</span>
     </div>`;
 });
+// message to client ends @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
